6f2e040858ee7c00930381f83987f918712fc2ae
Z_sakura <2923559561@qq.com>
2026-05-03 14:22:30 +0800

    Add files via upload

---
+++ Simulation_cd/API_KEY.py
@@ -0,0 +1,16 @@
+# API_KEY.py
+# 各种开放平台的KEY，不要外传
+
+# 零一万物大模型开放平台
+# https://platform.lingyiwanwu.com
+YI_KEY = ""
+
+SC_KEY = ""
+
+# 百度智能云千帆ModelBuilder
+# https://qianfan.cloud.baidu.com
+QIANFAN_ACCESS_KEY = ""
+QIANFAN_SECRET_KEY = ""
+
+# 百度智能云千帆AppBuilder-SDK
+APPBUILDER_TOKEN = ""
